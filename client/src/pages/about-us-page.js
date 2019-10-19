576e6dcf679001f30484981342d37508963972dc
Mohammed Elsayed <Mohammed.Elsayed41@myhunter.cuny.edu>
2019-10-19 19:54:44 -0400

    Small changes to about
    removed sidebar
    fixed columns

--- client/src/pages/about-us-page.js
+++ client/src/pages/about-us-page.js
@@ -1,16 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React, { Component } from 'react';
-import axios from 'axios';
-import "../css/about-us.css"
-
-
-import "bootstrap/dist/css/bootstrap.min.css"
+import "../css/about-us.css";
 import '../css/sidebar.css';
-import SideBar from '../components/sidebar-component';
-
-
-
-
-
 
 
 // import Oren from "../img/Oren_Senpai.jpg";
@@ -26,89 +17,53 @@ export default class AboutUs extends Component {
 
   render() {
     return (
-      <div>
+      <div>=
+        {/* Team Member 1 */}
+        <div className="about">
+          <div className="card border-0 shadow">
+            {/* <img src={Yong} className="card-img-top" alt="..." /> */}
+            <div className="card-body text-center">
+              <h5 className="card-title mb-0">Yong</h5>
+              <div className="card-text text-black-50">Built the backend and databases using the MERN stack</div>
+            </div>
+          </div>
+        </div>
+          
         
-        {/* Page Content */}            
-        <div className="container">
-          <div className="row">
 
-            <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3">
-              <SideBar/>
+        {/* Team Member 2 */}
+        <div className="about">
+          <div className="card border-0 shadow">
+            {/* <img src={Oren} className="card-img-top" alt="..." /> */}
+            <div className="card-body text-center">
+              <h5 className="card-title mb-0">Oren</h5>
+              <div className="card-text text-black-50">Built the backend and databases using MERN Stack</div>
             </div>
-          {/* /.row */}
           </div>
-          {/* /.container */}
         </div>
-
-          <div className="container justify-content-end">
-            <div className="row">
-
-              <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                <div className="bg-primary text-center py-4 mb-4">
-                  <h1 className="font-weight-light text-white">The Team</h1>
-                </div>
-              </div>
-
-              
-
-              {/* Team Member 1 */}
-              <div className="col-xl-9 col-lg-9 col-md-9 col-sm-9 mb-4">
-                <div className="card border-0 shadow">
-                  {/* <img src={Rifat} className="card-img-top" alt="..." /> */}
-                  <div className="card-body text-center">
-                    <h5 className="card-title mb-0">Young</h5>
-                    <div className="card-text text-black-50">Built the backend and databases using the MERN stack</div>
-                  </div>
-                </div>
-              </div>
-                
-              
-
-              {/* Team Member 2 */}
-              <div className="col-xl-9 col-lg-9 col-md-9 col-sm-9 mb-4 ml-5">
-                <div className="card border-0 shadow">
-                  {/* <img src={Oren} className="card-img-top" alt="..." /> */}
-                  <div className="card-body text-center">
-                    <h5 className="card-title mb-0">Oren</h5>
-                    <div className="card-text text-black-50">Built the backend and databases using MERN Stack</div>
-                  </div>
-                </div>
-              </div>
-              
-            </div>
-
-            
-
-            {/* Team Member 3 */}
-            <div className="col-xl-9 col-lg-9 col-md-9 col-sm-9 mb-4">
-              <div className="card border-0 shadow">
-                {/* <img src={Rehman} className="card-img-top" alt="..." /> */}
-                <div className="card-body text-center">
-                  <h5 className="card-title mb-0">Rehman</h5>
-                  <div className="card-text text-black-50">Designed the frontend with React and built routes using Express</div>
-                </div>
-              </div>
-            </div>
-            
-             
-            <div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 mb-4">
-            </div>
-
-            {/* Team Member 4 */}
-            <div className="col-xl-9 col-lg-9 col-md-9 col-sm-9 mb-4">
-              <div className="card border-0 shadow">
-                {/* <img src={Rehman} className="card-img-top" alt="..." /> */}
-                <div className="card-body text-center">
-                  <h5 className="card-title mb-0">Mohammed</h5>
-                  <div className="card-text text-black-50">Designed the frontend with React and built routes using Express</div>
-                </div>
-              </div>
-            </div>
-
-          {/* /.row*/}
+      
+      {/* Team Member 3 */}
+      <div className="about">
+        <div className="card border-0 shadow">
+          {/* <img src={Rehman} className="card-img-top" alt="..." /> */}
+          <div className="card-body text-center">
+            <h5 className="card-title mb-0">Rehman</h5>
+            <div className="card-text text-black-50">Designed the frontend with React and built routes using Express</div>
           </div>
-        {/* /.container*/}
         </div>
+      </div>
+      
+      {/* Team Member 4 */}
+      <div className="about">
+        <div className="card border-0 shadow">
+          {/* <img src={Momo} className="card-img-top" alt="..." /> */}
+          <div className="card-body text-center">
+            <h5 className="card-title mb-0">Mohammed</h5>
+            <div className="card-text text-black-50">Designed the frontend with React and built graphs using D3.js</div>
+          </div>
+        </div>
+      </div>
+  </div>
 
     );
   }
