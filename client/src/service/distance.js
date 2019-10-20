680801daf6aba2dda43f4c83d419c1fd23753722
OrenBen-Meir <orenbm11@gmail.com>
2019-10-20 03:01:55 -0400

    miles per gallon from user

--- client/src/service/distance.js
+++ client/src/service/distance.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 async function distance(from, to) {
-    const res = await axios.get(`http://open.mapquestapi.com/directions/v2/route?to=${to}&from=${from}&key=${'y1DKQh64ebrWc3jARWqhEZeAGQE72xPa'}`);
+    const res = await axios.get(`https://open.mapquestapi.com/directions/v2/route?to=${to}&from=${from}&key=${'y1DKQh64ebrWc3jARWqhEZeAGQE72xPa'}`);
     return res.data.route.distance;
 }
 
